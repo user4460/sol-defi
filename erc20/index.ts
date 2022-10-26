32e28f156aa0fb9541eabc99b1e8603297a47290
yoshiaki.n <user44604460@gmail.com>
2022-10-26 11:31:44 +0800

    ファイル整理

--- erc20/index.ts
+++
@@ -1,7 +0,0 @@
-import { default as tokens } from "./tokens";
-import abi from "./abi/ERC20.json";
-
-export = {
-  ...tokens,
-  abi,
-};
--- idle/index.ts
+++
@@ -1,8 +0,0 @@
-import { default as contracts } from "./contracts";
-import abi from "./abi/IdleTokenV3.json";
-
-export = {
-  ...contracts,
-  abi,
-  decimals: 18,
-};
