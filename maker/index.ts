c3ffda46168e68931cd8cfa99366e2d34fad4d1e
yoshiaki.n <user44604460@gmail.com>
2022-10-26 13:18:23 +0800

    ファイル整理

--- maker/index.ts
+++
@@ -1,9 +0,0 @@
-import { default as contracts } from "./contracts";
-import { default as ilks } from "./ilks";
-import { default as priceFeeds } from "./priceFeeds";
-
-export = {
-  ...contracts,
-  ...ilks,
-  ...priceFeeds
-};
